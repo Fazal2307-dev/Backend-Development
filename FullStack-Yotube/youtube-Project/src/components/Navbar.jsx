83463d08ffe718527e8b188faacfe6ca8c5312b2
Fazal2307-dev <mdfazal2307@gmail.com>
2025-10-23 22:36:15 +0530

    using tailwind in project

--- FullStack-Yotube/youtube-Project/src/components/Navbar.jsx
+++ FullStack-Yotube/youtube-Project/src/components/Navbar.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className='bg-gray-400 p-5 m-2 flex flex-row '>
+        <ol>
+            <li>Home</li>
+            <li>Pages</li>
+            <li>About</li>
+            <li>Support us</li>
+        </ol>
     </div>
   )
 }
