60cbc6cf713c051375c101288c06ff9e43e7422a
Fazal2307-dev <mdfazal2307@gmail.com>
2025-10-27 12:31:25 +0530

    checking how much cpu i have

--- 03HowNodeJsWork/file.js
+++ 03HowNodeJsWork/file.js
@@ -1,13 +1,16 @@
 const fs = require("fs");
 fs.writeFileSync("./contact.txt","fazal:+91454789");
 
-console.log("1");
-//Non Blocking
-// fs.readFile("./contact.txt","utf-8",(err,result)=>{
-//     console.log(result);
-// })
-//Block operations
-const result = fs.readFileSync("./contact.txt","utf-8");
-console.log(result);
-console.log("2");
-console.log("3")
+// console.log("1");
+// //Non Blocking
+// // fs.readFile("./contact.txt","utf-8",(err,result)=>{
+// //     console.log(result);
+// // })
+// //Block operations
+// const result = fs.readFileSync("./contact.txt","utf-8");
+// console.log(result);
+// console.log("2");
+// console.log("3")
+
+const os = require("os");
+console.log(os.cpus().length);
