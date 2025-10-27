ecd6f454fdf6305d9bbe76374a135d966da2c69d
Fazal2307-dev <mdfazal2307@gmail.com>
2025-10-27 12:24:22 +0530

    blocking.non-blocking

---
+++ 03HowNodeJsWork/file.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+fs.writeFileSync("./contact.txt","fazal:+91454789");
+
+console.log("1");
+//Non Blocking
+// fs.readFile("./contact.txt","utf-8",(err,result)=>{
+//     console.log(result);
+// })
+//Block operations
+const result = fs.readFileSync("./contact.txt","utf-8");
+console.log(result);
+console.log("2");
+console.log("3")
