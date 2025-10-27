7c50fdfd1a22ecd0437c70e54a307d536b1369a7
Fazal2307-dev <mdfazal2307@gmail.com>
2025-10-27 10:16:44 +0530

    async way of reading file in js

--- 02_FileHandling/file.js
+++ 02_FileHandling/file.js
@@ -4,5 +4,14 @@ const fs = require("fs");
 //async
 //fs.writeFile("./test.txt","async",(err)=>{});
 fs.writeFileSync("./contact.txt","fazal:+9199999999");
-const result = fs.readFileSync("./contact.txt","utf-8")
-console.log(result);
+//sync
+// const result = fs.readFileSync("./contact.txt","utf-8")
+// console.log(result);
+//async
+fs.readFile("./contact.txt","utf-8",(err,result)=>{
+    if(err){
+        console.log("Error",err)
+    }else{
+        console.log(result);
+    }
+});
