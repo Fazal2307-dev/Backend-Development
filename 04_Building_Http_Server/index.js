e7576e18563117ec9db79d3bf30c4ab35d99bb07
Fazal2307-dev <mdfazal2307@gmail.com>
2025-10-27 13:54:15 +0530

    http

--- 04_Building_Http_Server/index.js
+++ 04_Building_Http_Server/index.js
@@ -1,6 +1,8 @@
 const http = require("http")
+const fs = require("fs");
+fs.writeFileSync("./notex.txt","Learning about http server");
 const myServer = http.createServer((req,res)=>{
-    console.log("We get you reuest");
+  console.log(req);
     res.end("Heelo form server");
 })
 myServer.listen(8000,()=> console.log("Server Started!........"));
