e2618e552f05e42ad17517981229fb2cdfa916fe
Fazal2307-dev <mdfazal2307@gmail.com>
2025-10-28 20:52:56 +0530

    express js

--- 05_ExpressJs_with_nodejs/index.js
+++ 05_ExpressJs_with_nodejs/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+
 const app = express();
 app.get("/",(req,res)=>{
     return res.send("Hello From Home Page")
 });
 app.get("/about",(req,res)=>{
-    return res.send("Hello From About Page");
+    return res.send(`you name is ${req.query.name}` );
 })
+
 app.listen(8000,()=>console.log("Server Started....."));
