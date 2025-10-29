78a5dc8e7f68bd6ecc61ed9db9cee94864bef413
Fazal2307-dev <mdfazal2307@gmail.com>
2025-10-29 20:55:26 +0530

    middleware

--- Project_01/index.js
+++ Project_01/index.js
@@ -1,16 +1,30 @@
 const express = require("express");
 const app = express();
 const users = require("./MOCK_DATA.json");
+const fs = require("fs");
 const PORT = 8000;
 
-app.get("/users",(req,res)=>{
-    const html = `
-    <ul>
-    ${users.map((users)=> `<li>${users.first_name}, <li>${users.email} </li>,`)}
-    </ul>
-    `;
-    res.send(html);
+//MiddleWare-plugin
+app.use(express.urlencoded({extended:false}));
+
+app.use((req,res,next)=>{
+    console.log("First middle");
+    next();
 })
+app.use((req,res,next)=>{
+    console.log("second middle")
+    return res.end("hey");
+
+})
+
+// app.get("/users",(req,res)=>{
+//     const html = `
+//     <ul>
+//     ${users.map((users)=> `<li>${users.first_name}, <li>${users.email} </li>,`)}
+//     </ul>
+//     `;
+//     res.send(html);
+// })
 
 //Routes
 app.get("/",(req,res) =>{
@@ -27,21 +41,30 @@ app.get("/api/users/:id",(req,res)=>{
    return  res.json(user);
 })
 
-app.routes("/api/users/:id")
-.post("/api/users",(req,res) => {
-    //ToDo :create new user
-    return res.json({status:"pending"});
-});
+app.post("/api/users",(req,res)=>{
+//creating new users
+    const body = req.body;
+    users.push({...body, id : users.length + 1})
+    fs.writeFile("./MOCK_DATA.json",JSON.stringify(users),(err,data)=>{
+     return  res.json({status:'success',id:users.length })
+    });
+    
+})
+// app.route("/api/users/:id")
+// .get((req,res) => {
+//     //ToDo :create new user
+//     return res.json({status:"pending"});
+// });
 
-app.patch((req,res)=>{
-      //ToDo :Editing  user with id
-    return res.json({status:"pending"});
-});
+// app.patch((req,res)=>{
+//       //ToDo :Editing  user with id
+//     return res.json({status:"pending"});
+// });
 
-app.delete((req,res)=>{
-      //ToDo :delete user with id
-    return res.json({status:"pending"});
-});
+// app.delete((req,res)=>{
+//       //ToDo :delete user with id
+//     return res.json({status:"pending"});
+// });
 
 
 app.listen(PORT,()=>console.log(`Server Started at PORT: ${PORT} `));
