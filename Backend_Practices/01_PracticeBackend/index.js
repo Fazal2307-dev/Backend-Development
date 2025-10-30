2abb8bcf8abc40041d7ad30fc187f84b7b994a83
Fazal2307-dev <mdfazal2307@gmail.com>
2025-10-30 22:37:49 +0530

    practices backend

---
+++ Backend_Practices/01_PracticeBackend/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+app.get("/",(req,res)=>{
+    res.send("Hello from HomePage");
+})
+
+app.get("/support",(req,res)=>{
+    res.send("IF YOU NEED ANY HELP CONTACT ME")
+})
+app.get("/Internship",(req,res)=>{
+    res.send("you are in internship page");
+})
+app.get("/Jobs",(req,res)=>{
+    res.send("You are in jobs page")
+})
+
+app.get("/Competitions",(req,res)=>{
+    res.send(" you are in Competitions pages")
+})
+app.get("/Mentorships",(req,res)=>{
+    res.send("you are in mentorship pages")
+})
+app.get("/Practices",(req,res)=>{
+    res.send("you are in Practices pages");
+})
+app.post("/")
+
+app.listen(8000,()=>console.log("Server started..."))
