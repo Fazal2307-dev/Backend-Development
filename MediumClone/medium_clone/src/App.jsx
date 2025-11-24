9e33892d519dc7e3b2c1604b641b2591b1b2d6df
Fazal2307-dev <mdfazal2307@gmail.com>
2025-11-24 15:28:27 +0530

    feat: Initialize new React project with Vite and implement basic application structure with a Navbar component.

--- MediumClone/medium_clone/src/App.jsx
+++ MediumClone/medium_clone/src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Navbar from './Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
-      Hey
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/write" element={<Write />} />
+        <Route path="/topics" element={<Topics />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
     </div>
   )
 }
