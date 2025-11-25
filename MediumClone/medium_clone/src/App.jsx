6fe7e708eddfff85a5d38be246c95c6743cf8433
Fazal2307-dev <mdfazal2307@gmail.com>
2025-11-25 22:35:57 +0530

    Building medium

--- MediumClone/medium_clone/src/App.jsx
+++ MediumClone/medium_clone/src/App.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import Navbar from './Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Write from './pages/Write'
+import Topics from './pages/Topics'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/Sign_Up'
+import Support from './pages/Support'
 
 const App = () => {
   return (
@@ -12,6 +18,7 @@ const App = () => {
         <Route path="/topics" element={<Topics />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/support" element={<Support />} />
       </Routes>
     </div>
   )
